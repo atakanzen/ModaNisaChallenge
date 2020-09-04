425ce68ab44cf6a0e757857078289e48eb051449
Atakan <atakan@Atakans-MacBook-Air.local>
2020-09-05 01:19:45 +0300

    Wrote my first tests, got my reds as well.

--- client/tests/unit/todoslist.spec.js
+++ client/tests/unit/todoslist.spec.js
@@ -1,3 +1,22 @@
 import { shallowMount } from '@vue/test-utils';
+import ToDosList from '@/components/ToDosList.vue';
 
 // Displays ToDo Item List.
+
+let wrapper = null;
+
+beforeEach(() => {
+  wrapper = shallowMount(ToDosList);
+});
+
+afterEach(() => {
+  wrapper.destroy();
+});
+
+describe('ToDos List', () => {
+  it('renders todo item', () => {
+    const todoItems = wrapper.findAll('.todoitem');
+
+    expect(todoItems.length).toBeGreaterThanOrEqual(0);
+  });
+});
